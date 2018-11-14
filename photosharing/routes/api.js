952221bd02dc6c7e93ca6567ad24188ad8e6e038
Shane <shanemholbein@gmail.com>
2018-11-13 19:34:45 -0500

    set up API endpoints, profile, post, comment

--- photosharing/routes/api.js
+++ photosharing/routes/api.js
@@ -1,14 +1,30 @@
 var express = require('express')
 var router = express.Router()
-
+var controllers = require('../controllers')
 
 router.get('/:resource', function(req, res, next) {
   var resource = req.params.resource
 
-  res.json({
-    confirmation: 'success',
-    resource: resource
-
+  var controller = controllers[resource]
+  if (controller == null){
+    res.json({
+      confirmation: 'fail',
+      message: 'Invalid Resource'
+    })
+    return
+  }
+  controller.get(req.query, false)
+  .then(function(results){
+    res.json({
+      confirmation: 'success',
+      results: results
+    })
+  })
+  .catch(function(err){
+    res.json({
+      confirmation: 'fail',
+      message: err
+    })
   })
 
 })
